082bd3611a165c47cc9c909e3a73c5d746641595
ttupper92618 <ttupper@uxdesignassociates.com>
2021-08-11 14:16:58 -0500

    showing more details

--- src/components/Viewer/Viewer.tsx
+++ src/components/Viewer/Viewer.tsx
@@ -1,3 +1,4 @@
+import { IndeterminateCheckBox } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 import rem from "../../utils/rem";
@@ -6,15 +7,70 @@ import { ViewerProps } from "./Viewer.types";
 const ViewerContainer = styled.div<ViewerProps>`
   overflow: auto;
   height: calc(100vh - ${rem(9.75)});
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  padding: ${rem(2)};
+  letter-spacing: normal;
+  font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
 `;
 
-const ViewerFrame = styled.iframe`
+const ImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-color: #e2e2e2;
+  border-style: solid;
+  border-width: ${rem(0.125)};
+  padding: ${rem(0.5)};
+`;
+
+const Image = styled.img<ViewerProps>`
   width: 100%;
-  height: calc(100vh - ${rem(9.75)});
+`;
+
+const Title = styled.div`
+  color: #343434;
+  font-weight: 700;
+  font-size: ${rem(3)};
+  text-align: left;
+  padding-top: ${rem(2)};
+  width: 100%;
+`;
+
+const Author = styled.div`
+  color: #525252;
+  font-size: ${rem(1.5)};
+  text-align: left;
+  padding-top: ${rem(0.25)};
+  width: 100%;
 `;
 
 const Viewer: React.FC<ViewerProps> = ({ item }: ViewerProps) => {
-  return <ViewerContainer>{item?.url}</ViewerContainer>;
+  const getImageContainer = () => {
+    console.log(item);
+    if (item?.urlToImage) {
+      return (
+        <ImageContainer>
+          <Image src={item?.urlToImage}></Image>
+        </ImageContainer>
+      );
+    }
+  };
+
+  return (
+    <ViewerContainer>
+      {getImageContainer()}
+      <Title>{item?.title}</Title>
+      <Author>
+        <b>By: </b>
+        {item?.author}
+      </Author>
+    </ViewerContainer>
+  );
 };
 
 export default Viewer;
