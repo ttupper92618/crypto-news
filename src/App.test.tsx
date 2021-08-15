3952767cd462754e378cb3c3db838e26a9a4abeb
ttupper92618 <ttupper@uxdesignassociates.com>
2021-08-15 18:17:17 -0500

    fix app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./state/state/index";
 
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("It should mount", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
